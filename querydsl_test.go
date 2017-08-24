4faa6db331dce87c701934ec446a4ca584262f25
Ian McEwen <mian@cyverse.org>
2017-08-24 16:53:12 -0700

    add test for (*Query) Translate() (elastic.Query, error)

--- querydsl_test.go
+++ querydsl_test.go
@@ -34,12 +34,17 @@ func TestIsQuery_IsClause(t *testing.T) {
 	}
 }
 
-func TestTranslateClause(t *testing.T) {
+func addTestingClauseType() Clause {
 	AddClauseType("foo", func(args map[string]interface{}) (elastic.Query, error) {
 		return elastic.NewTermQuery("user", "arbitrary"), nil
 	})
 
-	clause := Clause{Type: "foo"}
+	return Clause{Type: "foo"}
+}
+
+func TestTranslateClause(t *testing.T) {
+	// TODO: test error conditions (no clause types, unsupported clause, etc.)
+	clause := addTestingClauseType()
 
 	translated, err := clause.Translate()
 	if err != nil {
@@ -62,4 +67,50 @@ func TestTranslateClause(t *testing.T) {
 	if userValue.(string) != "arbitrary" {
 		t.Errorf("term user query was %q rather than %q", userValue, "arbitrary")
 	}
+}
+
+func TestTranslateQuery(t *testing.T) {
+	clause := addTestingClauseType()
+
+	queryAll := Query{All: []*GenericClause{&GenericClause{Clause: &clause}}}
+	queryAny := Query{Any: []*GenericClause{&GenericClause{Clause: &clause}}}
+	queryNone := Query{None: []*GenericClause{&GenericClause{Clause: &clause}}}
+
+	testGivenQuery := func(query Query, subfield string) {
+		translated, err := query.Translate()
+		if err != nil {
+			t.Errorf("Translate failed with error: %q", err)
+		}
+		querySource, err := translated.Source()
+		if err != nil {
+			t.Errorf("Source get failed with error: %q", err)
+		}
+
+		boolQuery, ok := querySource.(map[string]interface{})["bool"]
+		if !ok {
+			t.Error("Source did not contain 'bool'")
+		}
+
+		section, ok := boolQuery.(map[string]interface{})[subfield]
+		if !ok {
+			t.Errorf("bool did not contain '%s'", subfield)
+		}
+
+		termQuery, ok := section.(map[string]interface{})["term"]
+		if !ok {
+			t.Error("subfield did not contain 'term'")
+		}
+
+		userValue, ok := termQuery.(map[string]interface{})["user"]
+		if !ok {
+			t.Error("term query did not contain 'user'")
+		}
+		if userValue.(string) != "arbitrary" {
+			t.Errorf("term user query was %q rather than %q", userValue, "arbitrary")
+		}
+	}
+
+	testGivenQuery(queryAll, "must")
+	testGivenQuery(queryAny, "should")
+	testGivenQuery(queryNone, "must_not")
 }
