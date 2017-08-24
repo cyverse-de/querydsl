fb02b84579cb2440418e82869e8f0b91e1565fa3
Ian McEwen <mian@cyverse.org>
2017-08-24 15:45:35 -0700

    add test for (*Clause) Translate() (elastic.Query, error)

--- querydsl_test.go
+++ querydsl_test.go
@@ -2,6 +2,8 @@ package querydsl
 
 import (
 	"testing"
+
+	"gopkg.in/olivere/elastic.v5"
 )
 
 func TestIsQuery_IsClause(t *testing.T) {
@@ -29,5 +31,35 @@ func TestIsQuery_IsClause(t *testing.T) {
 		if isClause != c.isClause {
 			t.Errorf("GenericClause{Query: &%+v, Clause: &%+v} returned %q from IsClause, not %q", c.query, c.clause, isClause, c.isClause)
 		}
+	}
+}
+
+func TestTranslateClause(t *testing.T) {
+	AddClauseType("foo", func(args map[string]interface{}) (elastic.Query, error) {
+		return elastic.NewTermQuery("user", "arbitrary"), nil
+	})
+
+	clause := Clause{Type: "foo"}
+
+	translated, err := clause.Translate()
+	if err != nil {
+		t.Errorf("Translate failed with error: %q", err)
+	}
+	querySource, err := translated.Source()
+	if err != nil {
+		t.Errorf("Source get failed with error: %q", err)
+	}
+
+	termQuery, ok := querySource.(map[string]interface{})["term"]
+	if !ok {
+		t.Error("Source did not contain 'term'")
+	}
+
+	userValue, ok := termQuery.(map[string]interface{})["user"]
+	if !ok {
+		t.Error("term query did not contain 'user'")
+	}
+	if userValue.(string) != "arbitrary" {
+		t.Errorf("term user query was %q rather than %q", userValue, "arbitrary")
 	}
 }
