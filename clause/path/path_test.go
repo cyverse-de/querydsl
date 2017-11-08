1e666d8a520d7ca7f6f35b21b3e0c96a6047ee9a
Ian McEwen <mian@cyverse.org>
2017-11-08 15:32:22 -0700

    Use subtests for path clause test

--- clause/path/path_test.go
+++ clause/path/path_test.go
@@ -1,6 +1,7 @@
 package path
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -16,34 +17,36 @@ func TestPathProcessor(t *testing.T) {
 	}
 
 	for _, c := range cases {
-		args := make(map[string]interface{})
+		t.Run(fmt.Sprintf("%T(%+v)", c.prefix, c.prefix), func(t *testing.T) {
+			args := make(map[string]interface{})
 
-		args["prefix"] = c.prefix
+			args["prefix"] = c.prefix
 
-		query, err := PathProcessor(args)
-		if c.shouldErr && err == nil {
-			t.Errorf("PathProcessor should have failed, instead returned nil error and query %+v", query)
-		} else if !c.shouldErr && err != nil {
-			t.Errorf("PathProcessor failed with error: %q", err)
-		} else if !c.shouldErr {
-			source, err := query.Source()
-			if err != nil {
-				t.Errorf("Source get failed with error: %q", err)
+			query, err := PathProcessor(args)
+			if c.shouldErr && err == nil {
+				t.Errorf("PathProcessor should have failed, instead returned nil error and query %+v", query)
+			} else if !c.shouldErr && err != nil {
+				t.Errorf("PathProcessor failed with error: %q", err)
+			} else if !c.shouldErr {
+				source, err := query.Source()
+				if err != nil {
+					t.Errorf("Source get failed with error: %q", err)
+				}
+
+				pQuery, ok := source.(map[string]interface{})["prefix"]
+				if !ok {
+					t.Error("Source did not contain 'prefix'")
+				}
+
+				pathQuery, ok := pQuery.(map[string]interface{})["path"]
+				if !ok {
+					t.Error("query did not contain 'path'")
+				}
+
+				if pathQuery.(string) != c.expectedQuery {
+					t.Errorf("query %q did not match expected value %q", pathQuery, c.expectedQuery)
+				}
 			}
-
-			pQuery, ok := source.(map[string]interface{})["prefix"]
-			if !ok {
-				t.Error("Source did not contain 'prefix'")
-			}
-
-			pathQuery, ok := pQuery.(map[string]interface{})["path"]
-			if !ok {
-				t.Error("query did not contain 'path'")
-			}
-
-			if pathQuery.(string) != c.expectedQuery {
-				t.Errorf("query %q did not match expected value %q", pathQuery, c.expectedQuery)
-			}
-		}
+		})
 	}
 }
